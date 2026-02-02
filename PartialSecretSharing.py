7e7c70f00660a34b4999bdd42640b5d8bab58895
Caleb Austin <calebraustin@live.com>
2026-02-02 17:51:47 -0600

    made indentation consistent

--- PartialSecretSharing.py
+++ PartialSecretSharing.py
@@ -35,86 +35,86 @@ def generatePrime(min):
 _RINT = functools.partial(random.SystemRandom().randint, 0)
 
 def _eval_at(poly, x, prime):
-    """Evaluates polynomial (coefficient tuple) at x, used to generate a
-    shamir pool in make_random_shares below.
-    """
-    accum = 0
-    for coeff in reversed(poly):
-        accum *= x
-        accum += coeff
-        accum %= prime
-    return accum
+	"""Evaluates polynomial (coefficient tuple) at x, used to generate a
+	shamir pool in make_random_shares below.
+	"""
+	accum = 0
+	for coeff in reversed(poly):
+		accum *= x
+		accum += coeff
+		accum %= prime
+	return accum
 
 def make_random_shares(secret, minimum, shares, prime):
-    """
-    Generates a random shamir pool for a given secret, returns share points.
-    """
-    if minimum > shares:
-        raise ValueError("Pool secret would be irrecoverable.")
-    poly = [secret] + [_RINT(prime - 1) for i in range(minimum - 1)]
-    points = [(i, _eval_at(poly, i, prime))
-              for i in range(1, shares + 1)]
-    return points
+	"""
+	Generates a random shamir pool for a given secret, returns share points.
+	"""
+	if minimum > shares:
+		raise ValueError("Pool secret would be irrecoverable.")
+	poly = [secret] + [_RINT(prime - 1) for i in range(minimum - 1)]
+	points = [(i, _eval_at(poly, i, prime))
+			  for i in range(1, shares + 1)]
+	return points
 
 def _extended_gcd(a, b):
-    """
-    Division in integers modulus p means finding the inverse of the
-    denominator modulo p and then multiplying the numerator by this
-    inverse (Note: inverse of A is B such that A*B % p == 1). This can
-    be computed via the extended Euclidean algorithm
-    http://en.wikipedia.org/wiki/Modular_multiplicative_inverse#Computation
-    """
-    x = 0
-    last_x = 1
-    y = 1
-    last_y = 0
-    while b != 0:
-        quot = a // b
-        a, b = b, a % b
-        x, last_x = last_x - quot * x, x
-        y, last_y = last_y - quot * y, y
-    return last_x, last_y
+	"""
+	Division in integers modulus p means finding the inverse of the
+	denominator modulo p and then multiplying the numerator by this
+	inverse (Note: inverse of A is B such that A*B % p == 1). This can
+	be computed via the extended Euclidean algorithm
+	http://en.wikipedia.org/wiki/Modular_multiplicative_inverse#Computation
+	"""
+	x = 0
+	last_x = 1
+	y = 1
+	last_y = 0
+	while b != 0:
+		quot = a // b
+		a, b = b, a % b
+		x, last_x = last_x - quot * x, x
+		y, last_y = last_y - quot * y, y
+	return last_x, last_y
 
 def _divmod(num, den, p):
-    """Compute num / den modulo prime p
+	"""Compute num / den modulo prime p
 
-    To explain this, the result will be such that:
-    den * _divmod(num, den, p) % p == num
-    """
-    inv, _ = _extended_gcd(den, p)
-    return num * inv
+	To explain this, the result will be such that:
+	den * _divmod(num, den, p) % p == num
+	"""
+	inv, _ = _extended_gcd(den, p)
+	return num * inv
 
 def _lagrange_interpolate(x, x_s, y_s, p):
-    """
-    Find the y-value for the given x, given n (x, y) points;
-    k points will define a polynomial of up to kth order.
-    """
-    k = len(x_s)
-    assert k == len(set(x_s)), "points must be distinct"
-    def PI(vals):  # upper-case PI -- product of inputs
-        accum = 1
-        for v in vals:
-            accum *= v
-        return accum
-    nums = []  # avoid inexact division
-    dens = []
-    for i in range(k):
-        others = list(x_s)
-        cur = others.pop(i)
-        nums.append(PI(x - o for o in others))
-        dens.append(PI(cur - o for o in others))
-    den = PI(dens)
-    num = sum([_divmod(nums[i] * den * y_s[i] % p, dens[i], p)
-               for i in range(k)])
-    return (_divmod(num, den, p) + p) % p
+	"""
+	Find the y-value for the given x, given n (x, y) points;
+	k points will define a polynomial of up to kth order.
+	"""
+	k = len(x_s)
+	assert k == len(set(x_s)), "points must be distinct"
+	def PI(vals):  # upper-case PI -- product of inputs
+		accum = 1
+		for v in vals:
+			accum *= v
+		return accum
+	nums = []  # avoid inexact division
+	dens = []
+	for i in range(k):
+		others = list(x_s)
+		cur = others.pop(i)
+		nums.append(PI(x - o for o in others))
+		dens.append(PI(cur - o for o in others))
+	den = PI(dens)
+	num = sum([_divmod(nums[i] * den * y_s[i] % p, dens[i], p)
+			   for i in range(k)])
+	return (_divmod(num, den, p) + p) % p
 
 def recover_secret(shares, prime):
-    """
-    Recover the secret from share points
-    (points (x,y) on the polynomial).
-    """
-    x_s, y_s = zip(*shares)
-    return _lagrange_interpolate(0, x_s, y_s, prime)
+	"""
+	Recover the secret from share points
+	(points (x,y) on the polynomial).
+	"""
+	x_s, y_s = zip(*shares)
+	return _lagrange_interpolate(0, x_s, y_s, prime)
 
 def escapableInput(prompt): 
 	try: 
@@ -249,8 +249,8 @@ def main():
 			secretNum = 0
 			power = 1
 			for char in secret:
-			    secretNum += char * power
-			    power *= 256
+				secretNum += char * power
+				power *= 256
 			while True: 
 				required = inputNaturalNumber('Required Parts: ')
 				total = inputNaturalNumber('Total Parts: ')
@@ -266,8 +266,8 @@ def main():
 
 			print('Partial Secrets:')
 			if shares:
-			    for share in shares:
-			        print('  ', share)
+				for share in shares:
+					print('  ', share)
 		elif action == '2': 
 			# temp values until we get the real values from a share
 			requiredBySet = None
@@ -288,16 +288,16 @@ def main():
 			secretNum = recover_secret(parts, prime)
 			secretList = []
 			while secretNum > 0: 
-			    secretList.append(secretNum % 256)
-			    secretNum //= 256
+				secretList.append(secretNum % 256)
+				secretNum //= 256
 			try: 
 				print('Secret:', bytearray(secretList).decode())
 			except: 
 				print('Failed to retrieve secret. ')
 		elif action != '3': 
-		    print('Please enter 1, 2, or 3. ')
-		    continue
+			print('Please enter 1, 2, or 3. ')
+			continue
 		break
 
 if __name__ == '__main__':
-    main()
+	main()
